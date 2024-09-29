9a9cce4686a85992cf0c9fb5b4b2abb9b3ab3c3e
shreya-paul-17 <cse.20211047@gmail.com>
2024-09-29 20:56:47 +0530

    Add modified files

--- empmgmtsys/src/main/java/com/shreya/controller/EmpController.java
+++ empmgmtsys/src/main/java/com/shreya/controller/EmpController.java
@@ -1,15 +1,20 @@
 package com.shreya.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shreya.entity.Employee;
 import com.shreya.service.EmpService;
 
-import jakarta.servlet.http.HttpSession;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class EmpController {
@@ -18,8 +23,10 @@ public class EmpController {
     private EmpService service;
 
     @GetMapping("/")
-    public String home()
+    public String home(Model m)
     {
+        List<Employee> emp=service.getAllEmp();
+        m.addAttribute("emp", emp);
         return "index";
     }
 
@@ -30,12 +37,48 @@ public class EmpController {
     }
 
     @PostMapping("/register")
-    public String empRegister(@ModelAttribute Employee e,HttpSession session){
+    // public String empRegister(@ModelAttribute Employee e,HttpSession session)
+    public String empRegister(@ModelAttribute Employee e, RedirectAttributes redirectAttributes){
         System.out.println(e);
         
         service.addEmp(e);
-        session.setAttribute("msg", "Employee added successfully...");
+        // session.setAttribute("msg", "Employee added successfully...");
+        redirectAttributes.addFlashAttribute("msg", "Employee added successfully...");
         return "redirect:/";
     } 
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model m)
+    {
+        Employee e =service.getEmpById(id);
+        m.addAttribute("emp", e);
+        return "edit";
+    }
+
+    // @PostMapping("/update")
+    // public String updateEmp(@ModelAttribute Employee e, HttpSession session)
+    // {
+    //     service.addEmp(e);
+    //     session.setAttribute("msg", "Employee Data updated successfully...");
+    //     return "redirect:/";
+    // }
+
+    @PostMapping("/update")
+    public String updateEmp(@ModelAttribute Employee e, RedirectAttributes redirectAttributes)
+    {
+        //System.out.println(e);
+        service.addEmp(e);
+        redirectAttributes.addFlashAttribute("msg", "Employee Data updated successfully...");
+        return "redirect:/";
+    } 
+
+    @GetMapping("/delete/{id}")
+    public String deleteEmp(@PathVariable int id, RedirectAttributes redirectAttributes)
+    {
+        service.deleteEmp(id);
+        redirectAttributes.addFlashAttribute("msg", "Employee Data deleted successfully...");
+        return "redirect:/";
+    }
+
 }
 
--- empmgmtsys/src/main/java/com/shreya/service/EmpService.java
+++ empmgmtsys/src/main/java/com/shreya/service/EmpService.java
@@ -1,5 +1,8 @@
 package com.shreya.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +18,25 @@ public class EmpService {
     public void addEmp(Employee e)
     {
         repo.save(e);
+    }
+
+    public List<Employee> getAllEmp()
+    {
+        return repo.findAll();
+    }
+
+    public Employee getEmpById(int id)
+    {
+        Optional<Employee> e=repo.findById(id);
+        if(e.isPresent())
+        {
+            return e.get();
+        }
+        return null; 
+    }
+
+    public void deleteEmp(int id)
+    {
+        repo.deleteById(id);
     }
 }
